27bbc686070ba42b8e6c8f8889d6b1edb3512961
Dylan <admin@patriotpixels.net>
2025-05-24 00:55:55 -0400

    feat: add computer choice

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //Rock paper scissors Game
 
+function getComputerChoice() {
+    let num = Math.random() * 3 
+    let num1 = Math.floor(num)
+    if (num1 === 0) {
+        return 'rock'
+    }
+    else if (num1 === 1) {
+        return 'paper'
+    }
+    else {
+        return 'scissors'
+    }
+}
+
+function getHumanChoice() {
+    prompt('Enter your choice')
+   
+}
+
+
+let humanScore;
+let computerScore;
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
